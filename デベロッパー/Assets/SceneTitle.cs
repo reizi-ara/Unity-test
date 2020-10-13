bcdb78210cecc68a0e1cc5c3d09af750b8c4280f
reizi_ara <xxxikuraxxx@gmail.com>
2020-10-13 15:27:28 +0900

    tes

--- デベロッパー/Assets/SceneTitle.cs
+++ デベロッパー/Assets/SceneTitle.cs
@@ -8,7 +8,8 @@ public class SceneTitle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //tes
+
     }
 
     // Update is called once per frame
